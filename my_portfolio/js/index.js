2e6f98cc1c39dfe8a46782b64bafdea82896e853
pigji <wjlqlrll@naver.com>
2024-10-25 18:07:31 +0900

    10.25_3nd

--- my_portfolio/js/index.js
+++ my_portfolio/js/index.js
@@ -9,3 +9,20 @@ let myFullpage = new fullpage("#fullpage", {
 
 //section요소 선택
 const section = document.querySelectorAll('section');
+
+
+
+/*--------------------------------------------------------*/
+//INTRO 텍스트 제목 한글자씩 타이핑 출력
+const homeText = document.querySelector('.introduction > .title > h1');
+const content = '안녕하세요!\n프론트엔드\u00a0개발자\n성지혜\u00a0입니다.';
+let count = 0;
+
+function typeing(){
+  homeText.innerText += content[count++];
+  if(count > content.length){
+    homeText.innerText = '';
+    count = 0;
+  }
+}
+setInterval(typeing, 400);
