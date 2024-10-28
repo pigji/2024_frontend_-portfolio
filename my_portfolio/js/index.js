4af8fa33bc4064cb76014406ba6745eabd048abc
pigji <wjlqlrll@naver.com>
2024-10-28 18:06:28 +0900

    10.28

--- my_portfolio/js/index.js
+++ my_portfolio/js/index.js
@@ -10,8 +10,6 @@ let myFullpage = new fullpage("#fullpage", {
 //section요소 선택
 const section = document.querySelectorAll('section');
 
-
-
 /*--------------------------------------------------------*/
 //INTRO 텍스트 제목 한글자씩 타이핑 출력
 const homeText = document.querySelector('.introduction > .title > h1');
@@ -26,3 +24,23 @@ function typeing(){
   }
 }
 setInterval(typeing, 400);
+
+/*--------------------------------------------------------*/
+/*프로젝트 이미지를 변경(썸네일 변경) */
+const navLi = document.querySelectorAll("#nav li a");
+
+//모든 a요소 클릭이벤트 추가
+navLi.forEach(item => item.addEventListener('click', (e) => {
+  e.preventDefault(); //a태그 기본링크 제거
+
+  //클릭한 a요소 href값을 img_url변수에 할당
+  const img_url = item.getAttribute('href');
+  console.log(img_url)
+
+  //window자식 요소인 img요소 scr 속성 값으로 img_url 할당
+  document.querySelector("#thumbanil img").setAttribute('src', img_url);
+  for(const item of navLi){
+    item.classList.remove('active');  //모든 a요소에 active 클래스 제거
+  }
+  item.classList.add('active'); //클릭한 a요소에 active클래스를 추가
+}))
